7fb87c475adcfb4fa4f8c0181bb86b0ecf2d4789
pupa228 <kirillarzamascev07@gmail.com>
2022-04-29 18:20:26 +0300

    Удалил ненужный импорт

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-from sqlighter import SQLighter
 
 from settings import *
 from private_data import TOKEN_TG, admin_password
