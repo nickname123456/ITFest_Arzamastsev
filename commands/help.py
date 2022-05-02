354d6853f4a427205483659425b255d5b1c05be0
pupa228 <kirillarzamascev07@gmail.com>
2022-05-02 21:21:02 +0300

    Обновил /help

--- commands/help.py
+++ commands/help.py
@@ -1,4 +1,10 @@
+from sqlighter import SQLighter
+db = SQLighter('it_fest.db')
+
 async def help(message):
     # Отправляем сообщение
-    await message.reply("Привет! Если у вас возникли какие-либо вопросы, то вот наши контакты:\nГруппа ВКонтакте Научим.online https://vk.com/nauchim.online\nСайт с мероприятиями https://www.научим.online")
-    await message.reply("Вот все мои команды:\n/start - регистрирует вас в базе данных, если вы в ней не зачислены\n/menu - Отправляет главное меню")
+    await message.answer("Привет! Если у вас возникли какие-либо вопросы, то вот наши контакты:\nГруппа ВКонтакте Научим.online https://vk.com/nauchim.online\nСайт с мероприятиями https://www.научим.online")
+    await message.answer("Вот все мои команды:\n/start - регистрирует вас в базе данных, если вы в ней не зачислены\n/menu - Отправляет главное меню")
+
+    if db.get_any(message.from_user.id, 'is_admin') == 1:
+        await message.answer("Специально для администраторов (только никому не говори): \n/add - добавить новый ивент \n/edit - изменить существующий ивент \n/delete - удалить ивент")
