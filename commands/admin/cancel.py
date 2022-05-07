d7c7419726106c4b4cb3a000b7a8436f59a3561a
pupa228 <kirillarzamascev07@gmail.com>
2022-05-07 18:46:17 +0300

    Закоментировал cancel.py

--- commands/admin/cancel.py
+++ commands/admin/cancel.py
@@ -1,10 +1,11 @@
+# Импортируем библиотеки
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 
-from commands.menu import menu
+from commands.menu import menu # Импортируем меню
 
 async def cancel(message: types.Message, state: FSMContext):
-    await state.finish()
+    await state.finish() # Завершаем действие
     await message.answer("Действие отменено")
 
-    await menu(message)
+    await menu(message) # Вызываем меню
