6283a5204a2197f3850a56ad3bb069cc91311f31
pupa228 <kirillarzamascev07@gmail.com>
2022-05-07 17:05:57 +0300

    Закомментил main.py

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ from settings import *
 from private_data import TOKEN_TG, admin_password
 
 from commands.admin.add_event import addEventState
+from commands.admin.edit_event import editEventState
 
+# Импортируем все команды
 from commands.start import start
 from commands.menu import menu
 from commands.help import help
@@ -25,7 +27,7 @@ from commands.admin.give_adm import give_adm
 from commands.admin.add_event import add_event_start, add_event_name, add_event_link, add_event_hashtag, add_event_description
 from commands.admin.cancel import cancel
 from commands.admin.delete_event import delete_event_kb, callback_delete
-from commands.admin.edit_event import edit_event_kb, edit_event_description,edit_event_hashtag,edit_event_link,edit_event_name, edit_event_start, editEventState
+from commands.admin.edit_event import edit_event_kb, edit_event_description,edit_event_hashtag,edit_event_link,edit_event_name, edit_event_start
 
 
 scheduler = AsyncIOScheduler()
@@ -33,6 +35,7 @@ scheduler = AsyncIOScheduler()
 bot = Bot(token=TOKEN_TG)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+# Подключение к бд
 db = SQLighter('it_fest.db')
 
 
@@ -47,6 +50,7 @@ async def process_start_command(message: types.Message):
 @dp.message_handler(commands=['menu', 'меню'], commands_prefix='/')
 async def process_menu_command(message: types.Message):
     user_id = message.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -60,6 +64,7 @@ async def process_menu_command(message: types.Message):
 @dp.message_handler(commands=['help', 'помощь'], commands_prefix='/')
 async def process_help_command(message: types.Message):
     user_id = message.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -73,6 +78,7 @@ async def process_help_command(message: types.Message):
 @dp.message_handler(commands=admin_password, commands_prefix='/')
 async def process_help_command(message: types.Message):
     user_id = message.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -82,10 +88,11 @@ async def process_help_command(message: types.Message):
     await give_adm(message)
 
 
-
+# Команда удаления ивента
 @dp.message_handler(commands=['delete', 'удалить'])
 async def process_help_command(message: types.Message):
     user_id = message.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -95,10 +102,11 @@ async def process_help_command(message: types.Message):
     await delete_event_kb(message)
 
 
-
+# Команда изменения ивента
 @dp.message_handler(commands=['edit', 'изменить'])
 async def process_help_command(message: types.Message):
     user_id = message.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -108,10 +116,11 @@ async def process_help_command(message: types.Message):
     await edit_event_kb(message)
 
 
-
+# Команда отмены
 @dp.message_handler(commands=['cancel', 'отмена'],state='*')
 async def process_help_command(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -125,6 +134,7 @@ async def process_help_command(message: types.Message, state: FSMContext):
 @dp.message_handler(commands=['add', 'addevent', 'добавить'])
 async def process_add_event_start(message: types.Message):
     user_id = message.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -133,9 +143,11 @@ async def process_add_event_start(message: types.Message):
     
     await add_event_start(message)
 
+# Получение названия нового ивента
 @dp.message_handler(state=addEventState.name)
 async def process_help_command(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -144,9 +156,11 @@ async def process_help_command(message: types.Message, state: FSMContext):
     
     await add_event_name(message, state)
 
+# Получение ссылки нового ивента
 @dp.message_handler(state=addEventState.link)
 async def process_help_command(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -155,9 +169,11 @@ async def process_help_command(message: types.Message, state: FSMContext):
     
     await add_event_link(message, state)
 
+# Получение хэштега нового ивента
 @dp.message_handler(state=addEventState.hashtag)
 async def process_help_command(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -166,9 +182,11 @@ async def process_help_command(message: types.Message, state: FSMContext):
     
     await add_event_hashtag(message, state)
 
+# Получение описания нового ивента
 @dp.message_handler(state=addEventState.description)
 async def process_help_command(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -178,10 +196,11 @@ async def process_help_command(message: types.Message, state: FSMContext):
     await add_event_description(message, state)
 
 
-
+# Получение названия изменяемого ивента
 @dp.message_handler(state=editEventState.name)
 async def process_help_command(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -190,9 +209,11 @@ async def process_help_command(message: types.Message, state: FSMContext):
     
     await edit_event_name(message, state)
 
+# Получение ссылки изменяемого ивента
 @dp.message_handler(state=editEventState.link)
 async def process_help_command(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -201,9 +222,11 @@ async def process_help_command(message: types.Message, state: FSMContext):
     
     await edit_event_link(message, state)
 
+# Получение хэштега изменяемого ивента
 @dp.message_handler(state=editEventState.hashtag)
 async def process_help_command(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -212,9 +235,11 @@ async def process_help_command(message: types.Message, state: FSMContext):
     
     await edit_event_hashtag(message, state)
 
+# Получение описания изменяемого ивента
 @dp.message_handler(state=editEventState.description)
 async def process_help_command(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -230,15 +255,17 @@ async def process_help_command(message: types.Message, state: FSMContext):
 
 
 
-
+# Обработка кнопки, когда у ивента нет хэштега
 @dp.callback_query_handler(lambda c: c.data and c.data == 'add_non_hashtag', state=addEventState.hashtag)
 async def process_callback_add_non_hashtag(callback_query: types.CallbackQuery, state = FSMContext):
     user_id = callback_query.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
         await callback_query.answer('Так.. Смотрю тебя нет в моей базе данных. Пожалуйста, напиши /start для того, чтобы я тебя зарегистрировал)')
         return
+    # Проверка на то, является ли юзер админом
     if db.get_any(user_id, 'is_admin') == 0:
         await callback_query.answer('Это команда доступна только администраторам! \n Если хочешь им стать, обратись к @Momfj')
         return
@@ -248,23 +275,26 @@ async def process_callback_add_non_hashtag(callback_query: types.CallbackQuery, 
     await bot.send_message(user_id, "Нет хэштега? Ну ничего страшного! Я буду рассылать все посты из указанного паблика. А теперь введи краткое описание ивента")
 
 
-
+# Обработка кнопки, когда у ивента остается что-то старое
 @dp.callback_query_handler(lambda c: c.data and c.data.startswith('edit_keep_'), state='*')
 async def process_callback_add_non_hashtag(callback_query: types.CallbackQuery, state = FSMContext):
     user_id = callback_query.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
         await callback_query.answer('Так.. Смотрю тебя нет в моей базе данных. Пожалуйста, напиши /start для того, чтобы я тебя зарегистрировал)')
         return
+    # Проверка на то, является ли юзер админом
     if db.get_any(user_id, 'is_admin') == 0:
         await callback_query.answer('Это команда доступна только администраторам! \n Если хочешь им стать, обратись к @Momfj')
         return
     
+    # объект равен ...
     obj = callback_query.data[10:]
     if obj == 'name':
-        await state.update_data(name='keep_old')
-        await editEventState.next()
+        await state.update_data(name='keep_old') # Задаем значение в хранилище
+        await editEventState.next() # Переходим на следующий этап
         await bot.send_message(user_id, "Хорошо, оставляю старое название. Теперь введи ссылку на сообщество вк", reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton('Оставить прежнию ссылку', callback_data=f'edit_keep_link')))
     elif obj == 'link':
         await state.update_data(link='keep_old')
@@ -278,10 +308,10 @@ async def process_callback_add_non_hashtag(callback_query: types.CallbackQuery, 
     elif obj == 'description':
         await state.update_data(description='keep_old')
         user_data = await state.get_data()
-        old_name = user_data['old_name']
+        old_name = user_data['old_name'] # Старое название
 
-        if user_data['link'] == 'keep_old': group_id = db.get_any_from_events('group_id',old_name)
-        else: group_id = user_data['link']
+        if user_data['link'] == 'keep_old': group_id = db.get_any_from_events('group_id',old_name) # Если надо оставить старую ссылку
+        else: group_id = user_data['link'] # Если не надо оставить старую ссылку
 
         if user_data['name'] == 'keep_old': name = db.get_any_from_events('name',old_name)
         else: name = user_data['name']
@@ -292,25 +322,28 @@ async def process_callback_add_non_hashtag(callback_query: types.CallbackQuery, 
         if user_data['description'] == 'keep_old': description = db.get_any_from_events('description',old_name)
         else: description = user_data['description']
 
+        # Изменяем ивент
         db.edit_any_from_events('group_id', old_name, group_id)
         db.edit_any_from_events('hashtag', old_name, hashtag)
         db.edit_any_from_events('description', old_name, description)
         db.edit_any_from_events('name', old_name, name)
 
+        # Создаем клавиатуру
         keyboard = InlineKeyboardMarkup().add(InlineKeyboardButton(f'{name}', callback_data=f'info_{name}'))
 
         await bot.send_message(user_id, "Твой ивент успешно обновлен! Хочешь посмотреть?", reply_markup=keyboard)
-        await state.finish()
+        await state.finish() # Заканчиваем
 
 
     
     
 
 
-# Обработка кэлбек кнопок с расылкой
+# Обработка кэлбек кнопок с подпиской
 @dp.callback_query_handler(lambda c: c.data and c.data.startswith('subscribe_'))
 async def process_callback_subscribe(callback_query: types.CallbackQuery):
     user_id = callback_query.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -324,6 +357,7 @@ async def process_callback_subscribe(callback_query: types.CallbackQuery):
 @dp.callback_query_handler(lambda c: c.data and c.data.startswith('info_'))
 async def process_callback_info(callback_query: types.CallbackQuery):
     user_id = callback_query.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -333,10 +367,11 @@ async def process_callback_info(callback_query: types.CallbackQuery):
     await callback_info(callback_query)
 
 
-
+# Обработка кнопки удаления ивента
 @dp.callback_query_handler(lambda c: c.data and c.data.startswith('delete_'))
 async def process_callback_delete(callback_query: types.CallbackQuery):
     user_id = callback_query.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -346,10 +381,11 @@ async def process_callback_delete(callback_query: types.CallbackQuery):
     await callback_delete(callback_query)
 
 
-
+# Обработка кнопки изменения ивента
 @dp.callback_query_handler(lambda c: c.data and c.data.startswith('edit_'))
 async def process_callback_delete(callback_query: types.CallbackQuery, state: FSMContext):
     user_id = callback_query.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -359,11 +395,12 @@ async def process_callback_delete(callback_query: types.CallbackQuery, state: FS
     await edit_event_start(callback_query, state)
 
 
-# Обработка всех остальных кэлбэк кнопок
+# Обработка всех остальных кнопок
 @dp.message_handler(Text(equals=["ℹИнформация", '✔Подписки', '🔓Доступные мероприятия']))
 @dp.callback_query_handler(lambda c: c.data)
 async def process_callback(callback_query: types.CallbackQuery=None):
     user_id = callback_query.from_user.id
+    # Проверка на то, есть ли юзер в бд
     try:
         db.get_any(user_id, 'id')
     except TypeError:
@@ -377,7 +414,7 @@ async def process_callback(callback_query: types.CallbackQuery=None):
 
 # Запускается при старте программы
 async def on_startup(dp):
-    # Информация про меня)
+    # Информация про меня))
     print('')
     print('-------------------------------')
     print('  Скрипт бота тг для итфеста запущен.')
@@ -389,7 +426,7 @@ async def on_startup(dp):
     print('')
 
     # Каждые 60 минут запускаем рассылку
-    scheduler.add_job(notification, "interval", minutes=1)
+    scheduler.add_job(notification, "interval", minutes=60)
 
 
 # Если запустили этот файл, как главный:
