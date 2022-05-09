c65ec4c85195adf02866a5a52f9948de9e016514
pupa228 <kirillarzamascev07@gmail.com>
2022-05-09 16:28:53 +0300

    Обновил help

--- commands/help.py
+++ commands/help.py
@@ -5,7 +5,6 @@ db = SQLighter('it_fest.db') # Подключаемся к бд
 
 async def help(message):
     # Отправляем сообщение
-    await message.answer("Привет! Если у вас возникли какие-либо вопросы, то вот наши контакты:\nГруппа ВКонтакте Научим.online https://vk.com/nauchim.online\nСайт с мероприятиями https://www.научим.online")
     await message.answer("Вот все мои команды:\n/start - регистрирует вас в базе данных, если вы в ней не зачислены\n/menu - Отправляет главное меню")
 
     if db.get_any(message.from_user.id, 'is_admin') == 1: # Если юзер - админ
